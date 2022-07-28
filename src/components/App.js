c206f5c2f02a3bcae41c644614a71bc0230cf9a7
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-28 20:33:41 -0300

    globalStyles como reset do CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import GlobalStyle from "./GlobalStyles";
 import Header from "./Header";
 import Movies from "./Movies";
+import Seats from "./Seats";
+import Success from "./Success";
+import Sessions from "./Sessions";
 
 export default function App () {
   return (
-    <BrowserRouter >
-    <Header />
-      <Routes >
-        <Route path="/" element={<Movies />} />
-      </Routes>
-    </BrowserRouter>
-
+    <>
+      <GlobalStyle />
+      <BrowserRouter >
+      <Header />
+        <Routes >
+          <Route path="/" element={<Movies />} />
+          <Route path="/sessoes/:idFilme" element={<Sessions />} />
+          <Route path="/sessoes/:idFilme/assentos/:idSessao" element={<Seats />} />
+          <Route path="/sucesso" element={<Success />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
