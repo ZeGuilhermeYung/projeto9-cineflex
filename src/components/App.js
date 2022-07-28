4147be25b86cfdc5e02c8e45f3621a17a95f5297
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-28 16:08:49 -0300

    componentes Header, Movies, Seats e Success

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import Header from "./Header";
+import Movies from "./Movies";
 
 export default function App () {
-  
+  return (
+    <BrowserRouter >
+    <Header />
+      <Routes >
+        <Route path="/" element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
 }
